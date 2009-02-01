173da6553a68ad317407cf67368dee58f1997f31
dsr <dan@rosspixelworks.com>
2009-02-01 17:14:24 -0600

    going to wxpython, able to pull and set copyright info with iptcinfo
    committer: dsr <dan@rosspixelworks.com>

--- iptc.py
+++ iptc.py
@@ -1,43 +1,22 @@
 #! /usr/local/bin/python
 
-import iptc
-from copyrighter import Ui_Form
-from PyQt4 import QtGui
-from PyQt4 import QtCore
+from iptcinfo import IPTCInfo
+import wx
 import sys
 
+# class TheWindow(wx.Frame):
+# 	def __init__(self, parent, id, title):
+# 		wx.Frame.__init__(self, parent, id, title, pos= (300,100), size= (400, 100))
+# 
+# 	
+# 
+# app = wx.PySimpleApp()
+# frame = TheWindow(None, -1, "CoPyrighter")
+# frame.Show(True)
+# app.MainLoop()
 
+info= IPTCInfo('test.jpg')
+print info
+info.data['copyright notice']= 'Steve Ross'
+info.saveAs('test_updated.jpg')
 
-class StartQT4(QtGui.QMainWindow):
-	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self, parent)
-		self.ui = Ui_Form()
-		self.ui.setupUi(self)
-
-		QtCore.QObject.connect(self.ui.button_image, QtCore.SIGNAL("clicked()"), self.open_image)
-		QtCore.QObject.connect(self.ui.button_folder, QtCore.SIGNAL("clicked()"), self.open_folder)
-		#QtCore.QObject.connect(self.ui.button_list, QtCore.SIGNAL("clicked()"), self.list_archive)
-	
-	
-	def open_image(self):
-		"""open a file to write copyright info"""
-		print 'open_image'
-		self.filename = QtGui.QFileDialog.getOpenFileName()
-		pass
-	
-
-	def open_folder(self):
-		"""open a folder to get images to write copyright info"""
-		print 'open_folder'
-		self.filenames = QtGui.QFileDialog.getExistingDirectory()
-		pass
-	
-
-
-
-#################################
-if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
-	myapp = StartQT4()
-	myapp.show()
-	sys.exit(app.exec_())
