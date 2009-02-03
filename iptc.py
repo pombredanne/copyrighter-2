8b18bc581ebe7d7337e8612326d49c03de54d31c
dsr <dan@rosspixelworks.com>
2009-02-02 18:35:22 -0600

    working on gui adding iptcinfo.py for reference 
    committer: dsr <dan@rosspixelworks.com>

--- iptc.py
+++ iptc.py
@@ -4,19 +4,27 @@ from iptcinfo import IPTCInfo
 import wx
 import sys
 
-# class TheWindow(wx.Frame):
-# 	def __init__(self, parent, id, title):
-# 		wx.Frame.__init__(self, parent, id, title, pos= (300,100), size= (400, 100))
-# 
-# 	
-# 
-# app = wx.PySimpleApp()
-# frame = TheWindow(None, -1, "CoPyrighter")
-# frame.Show(True)
-# app.MainLoop()
+class TheWindow(wx.Frame):
+	def __init__(self, parent, id, title):
+		wx.Frame.__init__(self, parent, id, title, pos= (300,300), size= (400, 100))
+	
+		#panel = wx.Panel(self, -1, (300,300))
+		hsizer = wx.BoxSizer(wx.HORIZONTAL)
+		vsizer = wx.BoxSizer(wx.VERTICAL)
+		text = wx.TextCtrl(self, -1,)
+		vsizer.Add(hsizer)
+		hsizer.Add(text, wx.ALL, border = 7)
+		hsizer.Add(wx.Button(self, -1, "Image"))
+		hsizer.Add(wx.Button(self, -1, "Directory"))
+		
+		self.SetSizer(hsizer)
+		info= IPTCInfo('test.jpg')
+		print info.data['copyright notice']
+	# info.data['copyright notice']= 'Steve Ross'
+	# info.saveAs('test_updated.jpg')
+	
 
-info= IPTCInfo('test.jpg')
-print info
-info.data['copyright notice']= 'Steve Ross'
-info.saveAs('test_updated.jpg')
-
+app = wx.PySimpleApp()
+frame = TheWindow(None, -1, "CoPyrighter")
+frame.Show(True)
+app.MainLoop()
