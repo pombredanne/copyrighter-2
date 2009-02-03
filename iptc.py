030a79f11bcf9dca5d67cd5de0a32b5f3a4a1c69
dsr <dan@rosspixelworks.com>
2009-02-02 21:07:27 -0600

    trying to get a border on things
    committer: dsr <dan@rosspixelworks.com>

--- iptc.py
+++ iptc.py
@@ -6,20 +6,17 @@ import sys
 
 class TheWindow(wx.Frame):
 	def __init__(self, parent, id, title):
-		wx.Frame.__init__(self, parent, id, title, pos= (300,300), size= (400, 100))
+		wx.Frame.__init__(self, parent, id, title, pos= (300,300), size= (400, 200))
 	
 		#panel = wx.Panel(self, -1, (300,300))
-		hsizer = wx.BoxSizer(wx.HORIZONTAL)
-		vsizer = wx.BoxSizer(wx.VERTICAL)
-		text = wx.TextCtrl(self, -1,)
-		vsizer.Add(hsizer)
-		hsizer.Add(text, wx.ALL, border = 7)
-		hsizer.Add(wx.Button(self, -1, "Image"))
-		hsizer.Add(wx.Button(self, -1, "Directory"))
-		
-		self.SetSizer(hsizer)
+		self.hsizer= wx.BoxSizer(wx.HORIZONTAL)
+		self.vsizer= wx.BoxSizer(wx.VERTICAL)
+		self.vsizer.Add(self.hsizer, border= 10)
+		self.button_image = wx.Button(self, -1, "Image")
+		self.hsizer.Add(self.button_image, border= 10)
 		info= IPTCInfo('test.jpg')
 		print info.data['copyright notice']
+		
 	# info.data['copyright notice']= 'Steve Ross'
 	# info.saveAs('test_updated.jpg')
 	
